0c5d8fe61486251a21b0015808743bf6610d27a9
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2021-10-29 12:17:12 +0300

    feat: получить данные по карточкам с сервера

--- mesto/src/components/App.js
+++ mesto/src/components/App.js
@@ -15,6 +15,7 @@ function App() {
   const [isPopupClose, handlePopupClose] = React.useState(false);
 
 
+
   function handleEditAvatarClick() {
     handlePopupClose(false);
     handleEditAvatarPopupOpen(true);
@@ -56,25 +57,6 @@ function App() {
           Сохранить
         </button>
       </PopupWithForm>
-      {/* <section className="popup popup-user">
-        <div className="popup__content" >
-          <button className="popup__close popup__close-user"  type="button"></button>
-          <h3 className="popup__title">Редактировать профиль</h3>
-          <form className="popup__form popup__form_users" name="submit-user">
-            <label className="popup__form-field">
-              <input type="text" id="name-input" className="popup__input popup__input_type_name"  placeholder="Имя" name="name" required minlength="2" maxlength="40"/>
-              <span id="name-input-error" className="popup__input-error"></span>
-            </label>
-            <label className="popup__form-field">
-              <input type="text" id="work-input"  placeholder="О себе" className="popup__input popup__input_type_work" name='about' required minlength='2'  maxlength='200'/>
-              <span id="work-input-error" className="popup__input-error"></span>
-            </label>
-            <button className="popup__submit-btn popup__submit-btn_edit"  type="submit">
-              Сохранить
-            </button>
-          </form>
-        </div>
-      </section> */}
       <PopupWithForm name="avatar" title="Обновить аватар" isOpen={isEditAvatarPopupOpen} onClose={isPopupClose} onClosePopup={closeAllPopups}>
         <label className="popup__form-field">
           <input type="url" id="url-input_avatar" placeholder="Ссылка на картинку" className="popup__input popup__input_type_avatar-img" name='link' required />
@@ -84,21 +66,6 @@ function App() {
           Сохранить
         </button>
       </PopupWithForm>
-      {/* <section className="popup popup-avatar">
-        <div className="popup__content popup__content_avatar" >
-          <button className="popup__close popup__close-avatar"  type="button"></button>
-          <h3 className="popup__title">Обновить аватар</h3>
-          <form className="popup__form popup__input-avatar" name="submit-element">
-            <label className="popup__form-field">
-              <input type="url" id="url-input_avatar" placeholder="Ссылка на картинку" className="popup__input popup__input_type_avatar-img" name='link' required />
-              <span id="url-input_avatar-error" className="popup__input-error"></span>
-            </label>
-            <button className="popup__submit-btn popup__submit-btn_avatar"  type="submit"> 
-              Сохранить
-            </button>
-          </form>
-        </div>
-      </section> */}
       <PopupWithForm name="add" title="Новое место" isOpen={isAddPlacePopupOpen} onClose={isPopupClose} onClosePopup={closeAllPopups}>
         <label className="popup__form-field">
           <input type="text" id="place-input"  placeholder="Название" className="popup__input popup__input_type_place-name" name='name' required minlength="2" maxlength="30" />
@@ -112,62 +79,13 @@ function App() {
           Создать
         </button>
       </PopupWithForm>
-      {/* <section className="popup popup-add">
-        <div className="popup__content" >
-          <button className="popup__close popup__close-add"  type="button"></button>
-          <h3 className="popup__title">Новое место</h3>
-          <form className="popup__form popup__input-add" name="submit-element">
-            <label className="popup__form-field">
-              <input type="text" id="place-input"  placeholder="Название" className="popup__input popup__input_type_place-name" name='name' required minlength="2" maxlength="30" />
-              <span id="place-input-error" className="popup__input-error"></span>
-            </label>
-            <label className="popup__form-field">
-              <input type="url" id="url-input" placeholder="Ссылка на картинку" className="popup__input popup__input_type_place-img" name='link' required />
-              <span id="url-input-error" className="popup__input-error"></span>
-            </label>
-            <button className="popup__submit-btn popup__submit-btn_add"  type="submit"> 
-              Создать
-            </button>
-          </form>
-        </div>
-    </section> */}
-      {/* <section className="popup popup_img">
-        <div className="popup__content popup__content_img" >
-          <button className="popup__close popup__close_img" type="button"></button>
-          <img src="<%=require('./images/element_Karachaevsk.jpg')%>" alt="Карачаево-Черкессия" className="popup__image" />
-          <h3 className="popup__title popup__title_img">Новое место</h3>
-        </div>
-  </section> */}
-
       <ImagePopup />
       <PopupWithForm name="delete" title="Вы уверены?" onClose={isPopupClose} onClosePopup={closeAllPopups}>
         <button className="popup__submit-btn popup__submit-btn_delete"  type="submit">
           Да
         </button> 
       </PopupWithForm>
-      {/* <section className="popup popup_delete">
-        <div className="popup__content popup__content_delete" >
-          <button className="popup__close popup__close_delete" type="button"></button>
-          <h3 className="popup__title popup__title_delete">Вы уверены?</h3>
-          <button className="popup__submit-btn popup__submit-btn_delete"  type="submit">
-            Да
-          </button> 
-        </div>
-      </section> */}
     </div>
-    <template id="element-template">
-      <article className="element">
-        <img src="<%=require('./images/element_Karachaevsk.jpg')%>" alt="Карачаево-Черкессия" className="element__image" />
-        <button className="element__remove-button"  type="button" ></button>
-        <div className="element__title-block">
-          <p className="element__title">Карачаево-Черкессия</p>
-          <div className="element__likes">
-            <button className="element__heart-button"  type="button"></button>
-            <p className="element__likes-number">0</p>
-          </div>
-        </div>
-      </article>
-    </template>
   </div>
   );
 }
--- mesto/src/components/Main.js
+++ mesto/src/components/Main.js
@@ -1,6 +1,4 @@
-import React from 'react';
-// Пути к изображениям внутри сборки
-import avatarPath from '../images/Avatar.jpg'; 
+import React from 'react'; 
 //
 import API, { apiData } from '../utils/Api';
 
@@ -9,6 +7,7 @@ function Main(props) {
   const [userName, handleGetUserName] = React.useState(null);
   const [userDescription, handleGetUserDescription] = React.useState(null);
   const [userAvatar, handleGetUserAvatar] = React.useState(null);
+  const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
     // запрос в API за пользовательскими данными
@@ -23,6 +22,34 @@ function Main(props) {
       return [];
     })
   });
+
+  React.useEffect(() => {
+    // запрос в API за карточками мест
+    apiData.getInitialCards()
+    .then(items => {
+      console.log(items);
+      setCards(items);
+    })
+    .catch((err) => {
+      console.log(err); // "Что-то пошло не так: ..."
+      return [];
+    });
+    return (
+      cards.forEach(item => {
+        <article className="element">
+        <img src={item.link} alt={item.name} className="element__image" />
+        <button className="element__remove-button"  type="button" ></button>
+        <div className="element__title-block">
+          <p className="element__title">{item.name}</p>
+          <div className="element__likes">
+            <button className="element__heart-button"  type="button"></button>
+            <p className="element__likes-number">{item.likes.length}</p>
+          </div>
+        </div>
+      </article>
+      })
+    )
+  }, []);
 
   return (
     <main>
