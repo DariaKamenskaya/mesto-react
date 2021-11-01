47e466ec903f4738799348b10815d344f2dbd530
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2021-11-01 14:56:14 +0300

    refactor: сделать класс Card функциональной компонентой

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,35 +1,30 @@
 import React from 'react';
 
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    {/* this.card = this.props.card; */}
-    this.handleClick = this.handleClick.bind(this);
-  }
+function Card(props) {
 
-  handleClick()  {
-    this.props.onCardClick(this.props.card);
-  }  
+  handleClick = handleClick.bind(this);
 
+  function  handleClick()  {
+    props.onCardClick(props.card);
+  } 
 
-  render() {
-    return(
-      <article className="element">
-        <img src={this.props.card.link} alt={this.props.card.name} className="element__image"  onClick={this.handleClick} />
-        <button className="element__remove-button"  type="button" ></button>
-        <div className="element__title-block">
-          <p className="element__title">{this.props.card.name}</p>
-          <div className="element__likes">
-            <button className="element__heart-button"  type="button"></button>
-            <p className="element__likes-number">{this.props.card.likes.length}</p>
-          </div>
+  return(
+    <article className="element">
+      <img src={props.card.link} alt={props.card.name} className="element__image"  onClick={handleClick} />
+      <button className="element__remove-button"  type="button" ></button>
+      <div className="element__title-block">
+        <p className="element__title">{props.card.name}</p>
+        <div className="element__likes">
+          <button className="element__heart-button"  type="button"></button>
+          <p className="element__likes-number">{props.card.likes.length}</p>
         </div>
-      </article>
-    );
-  }
-
-
+      </div>
+    </article>
+  );
 
 }
+
+
+
   
 export default Card; 
