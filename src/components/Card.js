426071e089c6ea71ad89b3efa28b45dfe6ac5737
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2021-11-06 11:27:34 +0300

    feat: добавить возможность видеть лайк своих карточек

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,6 +12,14 @@ function Card(props) {
   ` ${isOwn ? 'element__remove-button' : 'element__remove-button_hidden'}`
   ); 
 
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = props.card.likes.some(i => i._id === props.currentUser._id);
+
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = (
+    `element__heart-button  ${isLiked ? 'element__heart-button-active' : ' '}`
+  );
+
   function  handleClick()  {
     props.onCardClick(props.card);
   } 
@@ -25,7 +33,7 @@ function Card(props) {
       <div className="element__title-block">
         <p className="element__title">{props.card.name}</p>
         <div className="element__likes">
-          <button className="element__heart-button"  type="button"></button>
+          <button className={cardLikeButtonClassName}  type="button"></button>
           <p className="element__likes-number">{props.card.likes.length}</p>
         </div>
       </div>
